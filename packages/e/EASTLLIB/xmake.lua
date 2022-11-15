6a6292204027f72d91fb7fc6dfd7905f34b4d4d2
coderling <coderling@gmail.com>
2022-11-15 18:48:35 +0800

    add defines

--- packages/e/EASTLLIB/xmake.lua
+++ packages/e/EASTLLIB/xmake.lua
@@ -3,6 +3,10 @@ package("EASTLLIB")
     on_load(function (package)
         package:set("installdir", path.join(os.scriptdir(), package:plat(), package:arch(), package:mode()))
         package:add("defines", "EASTL_USER_DEFINED_ALLOCATOR", "EASTDC_USE_STANDARD_NEW=1")
+
+        if package:mode() == "debug" or package:mode() == "releasedbg" then--is_mode("debug") or is_mode("releasedbg") then
+            target:add("defines", "EASTL_NAME_ENABLED")
+        end
     end)
 
     on_fetch(function (package)
