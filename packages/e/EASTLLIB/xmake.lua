cb1b3fd2a94d5271492299037221af60850ab108
coderling <coderling@gmail.com>
2022-11-15 18:45:38 +0800

    rebuild all add eastdc

--- packages/e/EASTLLIB/xmake.lua
+++ packages/e/EASTLLIB/xmake.lua
@@ -2,6 +2,7 @@ package("EASTLLIB")
     set_description("pre build eastl")
     on_load(function (package)
         package:set("installdir", path.join(os.scriptdir(), package:plat(), package:arch(), package:mode()))
+        package:add("defines", "EASTL_USER_DEFINED_ALLOCATOR", "EASTDC_USE_STANDARD_NEW=1")
     end)
 
     on_fetch(function (package)
